5c17ee9c855e18516471d2f40d6de449fb47bdda
Marwan Zaky <marouaneezzaky@gmail.com>
2021-11-23 03:52:58 +0100

    Removing the Cinemachine controller, and using a custom script controller instead

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+namespace Packtool
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        float xRotation = 0f;
+        float yRotation = 0f;
+
+        public Transform playerBody;
+        public Vector3 playerBodyOffset;
+
+        [Header("Other Settings")]
+        public float minXRotation = -90f;
+        public float maxXRotation = 90f;
+        public float mouseSensivity = 100f;
+
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        void Update()
+        {
+            MoveCamera();
+        }
+
+        void LateUpdate()
+        {
+            FollowTarget();
+        }
+
+        void FollowTarget()
+        {
+            var targetPos = playerBody.position + playerBodyOffset;
+            transform.position = targetPos;
+        }
+
+        void MoveCamera()
+        {
+            var mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+            var mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+            yRotation += mouseX;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, minXRotation, maxXRotation);
+
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        }
+    }
+}
--- Assets/Package/Weapons/Scripts/GunWeapon.cs
+++ Assets/Package/Weapons/Scripts/GunWeapon.cs
@@ -4,20 +4,36 @@ namespace MarwanZaky
 {
     public class GunWeapon : Weapon
     {
+        Camera cam;
+
         [SerializeField] GameObject bulletPrefab;
         [SerializeField] Transform laser;
+        [SerializeField] LayerMask layerMask;
+
+        private void Start()
+        {
+            cam = Camera.main;
+        }
 
         protected override void Attack()
         {
             base.Attack();
 
             if (!IsAttacking)
-                SpawnBullet();
+                Fire();
         }
 
-        void SpawnBullet()
+        void Fire()
         {
-            Instantiate(bulletPrefab, laser.position, laser.rotation);
+            var aimPos = new Vector2((float)Screen.width / 2f, (float)Screen.height / 2f);
+            var ray = Camera.main.ScreenPointToRay(aimPos);
+            var hit = new RaycastHit();
+
+            Physics.Raycast(ray.origin, ray.direction, out hit, 1000f, layerMask);
+
+            var targetDir = (hit.point - laser.position).normalized;
+
+            Instantiate(bulletPrefab, laser.position, Quaternion.LookRotation(targetDir));
         }
     }
 }
